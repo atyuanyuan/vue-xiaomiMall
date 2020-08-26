d7805b106a6879ae4c405a928010d2d401720f13
shiyuanyuan <wyyxshiyuan@163.com>
2020-08-26 11:12:14 +0800

    目录结构设置

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'nav-header',
+    props: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {},
+    mounted() {},
+    watch: {},
+    methods: {},
+    components: {},
+};
+</script>
+<style scoped lang="scss">
+</style>
---
+++ src/pages/order.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ordedr',
+    props: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {},
+    mounted() {},
+    watch: {},
+    methods: {},
+    components: {},
+};
+</script>
+<style scoped lang="scss">
+</style>
